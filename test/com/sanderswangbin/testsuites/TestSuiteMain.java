e3d5292f022128832bcf53bdde5de2c9c0b0e9de
SandersWangBin <sanderswangbin@gmail.com>
2017-07-04 12:33:46 +0200

    JSONCK create JSONCK from PULL
    Signed-off-by: SandersWangBin <sanderswangbin@gmail.com>

---
+++ test/com/sanderswangbin/testsuites/TestSuiteMain.java
@@ -0,0 +1,11 @@
+package com.sanderswangbin.testsuites;
+
+import com.sanderswangbin.testcases.jsonck.TestCaseJsonCK;
+
+public class TestSuiteMain {
+
+	public static void main(String... argv) {
+		System.out.println(new TestCaseJsonCK().test());
+	}
+
+}
